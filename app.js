83d13c36bc421db2196aa4a231a348c283acb4a9
analuizamamaro-netizen <analuiza.m.amaro@gmail.com>
2025-11-07 11:38:28 +0100

    Basis for our website done in VS code

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById("loadData").addEventListener("click", () => {
+  const output = document.getElementById("output");
+  output.innerHTML = "<p>Button clicked! JavaScript is working 🎉</p>";
+});
+
+
